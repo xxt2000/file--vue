4e4d818f5811eec7a141ac5291d141ac880249c4
tuyuqi <910551163@qq.com>
2026-05-05 10:14:28 +0800

    恢复了main.js

--- src/main.js
+++ src/main.js
@@ -9,9 +9,9 @@ import router from './router'
 const app = createApp(App)
 
 // 注册所有图标
-// for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-//     app.component(key, component)
-// }
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 
 app.use(createPinia())
 app.use(router)
